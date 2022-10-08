f774bf7ad766fc6c2493637fd9aa7d3b3e2c11a2
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-10-08 11:55:33 +0300

    matamatiksal

---
+++ matamatiksel operatorelar.py
@@ -0,0 +1,24 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+# toplama
+print(5397+6874)
+# çıkarma
+print(91000/63)
+#çarpma
+print(55*23)
+#bölme
+print(54-89)
+#üs alma
+print(65**78)
+# modalma
+print(50%9)
+#tam bölme
+print(195//7)
+print(-85//3)
