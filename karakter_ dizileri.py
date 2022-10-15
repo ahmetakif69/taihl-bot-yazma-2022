098cc142dcc6353b45f02c80fe245870a8267a5e
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-10-15 11:30:46 +0300

    karaktaer

---
+++ karakter_ dizileri.py
@@ -0,0 +1,10 @@
+# x=str(input("adınız:"))
+# y=str(input("yaşınız"))
+# print(x)
+# print(y)
+yas=14
+isim="ahmet"
+x=input("adınız")
+y=input("yaşınız")
+print("adınız:" + x , " yaşınız" + y)
+print(isim[0])
