065158d9ca0e13666d7ec6c1d53a421f69f8c77a
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-10-15 09:58:02 +0300

    Create degişken tanimlama.py

---
+++ degişken tanimlama.py
@@ -0,0 +1,34 @@
+# Değişkenler olmadan 2 öğrencinin sınav notlarını hesaplayalım
+# print( (80*0.4) + (75*0.6) )
+# print( (45*0.4) + (60*0.6) )
+# değişken kullanarak yüzde hesaplama
+x=0.4
+y=0.6
+
+print((80*x)+(75*y))
+print((60*x)+(45*y))
+# değişken kuralları
+# rakam ile başlayamaz
+# 1sayı hata verdi
+sayi1=85
+print(sayi1)
+# büyük küçük harfe duyarlıdır
+number=12
+NUMBER=15
+print(number)
+print(NUMBER)
+# TÜRKÇE KARAKTER KULLANILAMAZ
+# YAŞ =18  HATALI KULLANIM
+yas=18 #türkçe harf olmadan türkçe kullanabilir(tavsiye edilmez)
+age=17
+print(yas)
+print(age)
+# değişken verilerini birleştirme
+x=1 # integer
+y=1.2 # float
+ad="ahmet" # string
+sinav_basarili_mi=True #boolean
+print(x+y)
+# print(x+ad) hata verdi
+print(x+sinav_basarili_mi)
+# print(ad+sinav_basarili_mi) hata verdi
