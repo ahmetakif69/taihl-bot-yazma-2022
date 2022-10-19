b057d08f815c831ebf8f650e5a16e01b080dbf39
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-10-19 21:53:00 +0300

    hocam ödevi yaptım

---
+++ odev1.py
@@ -0,0 +1,40 @@
+url = "https://teknolojiaihl.meb.k12.tr"
+
+# 1- "url" içinde kaç karakter olduğunu yazdır.
+# print(len(url))
+
+# 2- "url" içindeki "meb" sözcüğünü ekrana yazdırın.
+
+# 3- "url" içindeki "k12" sözcüğünü ekrana yazdırın.
+
+# 4- Kullanıcıdan adını, en sevdiği yemeği alın ve cümle içinde yazdırın.
+# ÖRNEK CÜMLE: Adınız: Eren. En sevdiğiniz yemek: Pırasa
+
+# 5- Öğrencinin 2 sınav notunu alın. Notunu hesaplayıp cümle içinde yazdırın.
+# 1. sınav oranı: %35
+# 2. sınav oranı: %65
+# ÖRNEK CÜMLE: 1. sınav: 70 puan, 2. sınav: 90 puan, Notunuz: 83.0
+
+# 6- Kullanıcının adını alın ve yan yana 100 defa yazdırın.
+# 1.odev
+print(len(url))
+# 2.odev
+print(url[23])
+print(url[24])
+print(url[25])
+# 3.odev
+print(url[27])
+print(url[28])
+print(url[29])
+# 4.odev
+isim = input("İsminiz: ")
+yemek  = input("en sevdiğiniz yemek: ")
+
+print("Adınız: " + isim + " en sevdiğiniz yemek:  "+yemek )
+# 5.odev
+x = 0.35
+y = 0.65
+print((45*x)+(89*y))
+# 6.odev
+isim = input("İsminiz: ")
+print(isim*100)
