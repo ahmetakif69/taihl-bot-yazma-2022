ce6ba8dc24c239208562d6c42ca1dffea62f6330
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-12-31 12:10:52 +0300

    ajjjj

--- sayfa_eleman_secicileri.py
+++ sayfa_eleman_secicileri.py
@@ -1,6 +1,7 @@
 #tarayivimixi ixport edelim
 from moduller.tarayici import Tarayici
 from selenium.webdriver.common.by import  By
+from time import sleep
 tarayici_nesnesi = Tarayici()
 tarayici= tarayici_nesnesi.al()
 # =okulun internet sitesine gidelim
@@ -16,3 +17,28 @@ print(baslik)
 
 
 baslik.screenshot("./gorseller/baslik.png")
+
+arama_kutusu   =  tarayici.find_element(By.CSS_SELECTOR,"#araTextBox")
+arama_kutusu.send_keys("İnovasyon")
+sleep(2)
+
+
+arama_tus  =  tarayici.find_element(By.ID,"araButton")
+# arama_tus.click()
+# sleep(3)
+
+twitter_title=tarayici.find_element(By.NAME,"twitter:title")
+print(twitter_title.get_attribute("content"))
+
+okullarimiz=tarayici.find_element(By.LINK_TEXT,"Okullarımız")
+# okullarimiz.click()
+# sleep(3)
+
+aile_birligi=tarayici.find_element(By.PARTIAL_LINK_TEXT,"Aile Birliği")
+# aile_birligi.click()
+# sleep(2)
+linkler=tarayici.find_elements(By.TAG_NAME,"a")
+# print(len(linkler))
+
+iletisim=tarayici.find_element(By.XPATH,'//*[@id="o_hakkinda"]')
+iletisim.screenshot("./gorseller/iletisim.png")
