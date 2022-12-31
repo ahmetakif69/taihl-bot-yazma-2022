7661187f5e5124a11e7f448afcb63147bb77c39c
ahmetakif69 <114801931+ahmetakif69@users.noreply.github.com>
2022-12-31 11:21:43 +0300

    ashgghghghgghhgghghhg

--- sayfa_eleman_secicileri.py
+++ sayfa_eleman_secicileri.py
@@ -1,3 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#tarayivimixi ixport edelim
+from moduller.tarayici import Tarayici
+from selenium.webdriver.common.by import  By
+tarayici_nesnesi = Tarayici()
+tarayici= tarayici_nesnesi.al()
+# =okulun internet sitesine gidelim
+tarayici.get("https://teknolojiaihl.meb.k12.tr/")
+tarayici.maximize_window()
+
+
+
+baslik=tarayici.find_element(By.CLASS_NAME,"container")
+
+
+print(baslik)
+
+
+baslik.screenshot("./gorseller/baslik.png")
